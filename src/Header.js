240c98ae5de93af95f1c7a43c6324b177196db63
jjuarez5 <julio.juarez5@outlook.com>
2023-03-25 23:57:17 -0500

    add links in header

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <div className="w-full sticky -top-0 z-0 font-semibold inline-flex flex-1 bg-blue-900 text-zinc-100 p-1 drop-shadow-lg">
       <h1 className="text-5xl">G&G's Catering</h1>
       <ul className="inline-flex flex-1 justify-center text-lg space-x-8 mt-3">
         <li>
-          <h2>link</h2>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <h2>link</h2>
+          <Link to="/menu">Menu</Link>
         </li>
         <li>
-          <h2>link</h2>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
     </div>
